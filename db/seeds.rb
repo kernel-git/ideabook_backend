b37ef5918b60600f4a192378f5dff2ccb2744944
boss078 <36530564+boss078@users.noreply.github.com>
2021-07-15 19:21:01 +0300

    Removed db:reset from seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Rake::Task['db:reset'].invoke
-
 Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each do |seed|
   load seed
 end

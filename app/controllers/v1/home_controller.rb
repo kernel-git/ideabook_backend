0bab44f39617f8d3324ce9c942bc0237fa25996f
boss078 <36530564+boss078@users.noreply.github.com>
2021-07-29 01:14:51 +0300

    Added some unsaved changes

--- app/controllers/v1/home_controller.rb
+++ app/controllers/v1/home_controller.rb
@@ -2,6 +2,9 @@
 
 class V1::HomeController < ApplicationController
   before_action :authenticate_user!
+  skip_after_action :verify_policy_scoped, :only => :index
+  skip_after_action :verify_authorized, :except => :index
+
 
   def show
     render json: { message: "If you see this, you're in!" }
